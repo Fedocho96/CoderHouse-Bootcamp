ae6573823728f10b37882532de546180c306d830
Federico Rosales <fedocho.96@gmail.com>
2022-05-16 23:36:37 -0300

    Lista tarea 7 y api de mocha, sigue el problema que renderiza itemdetail antes de cargar productos

--- src/components/itemlist/ItemList.js
+++ src/components/itemlist/ItemList.js
@@ -5,7 +5,7 @@ import Item from '../item/Item'
 const ItemList = ({manageCart, stock, listaproductos}) => {
   return (
       <>
-        <div className=' container grid md:grid-cols-2 lg:grid-cols-4 gap-3 h-max'>
+        <div className=' container grid md:grid-cols-2 lg:grid-cols-4 gap-3 h-max rounded-sm'>
           {
             listaproductos.map((producto)=><Item producto={producto} key={producto.id} manageCart={manageCart} stock={stock} />)
           }
--- src/components/itemlistcontainer/ItemListContainer.js
+++ src/components/itemlistcontainer/ItemListContainer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 /* ------------------------------- containters ------------------------------- */
 import ItemList from '../itemlist/ItemList'
@@ -7,29 +8,18 @@ import ItemList from '../itemlist/ItemList'
 const ItemListContainer = ({manageCart, stock}) => {
     const [listaproductos, setListaProductos] = useState([])
     
-  const productos = [
-      {id: '1', title: 'lechuga1', description:' description description description description description description description ', price:'20', pictureUrl:'https://www.cocinayvino.com/wp-content/uploads/2020/09/www.cocinayvino.com-la-lechuga-es-mucho-mas-que-agua-lechuga1-e1601317319527-1200x900.jpg'},
-      {id: '2', title: 'lechuga2', description:'description description description description description description description description description description ', price:'20', pictureUrl:'https://www.cocinayvino.com/wp-content/uploads/2020/09/www.cocinayvino.com-la-lechuga-es-mucho-mas-que-agua-lechuga1-e1601317319527-1200x900.jpg'},
-      {id: '3', title: 'lechuga3', description:'description description description description description description description description description ', price:'20', pictureUrl:'https://www.cocinayvino.com/wp-content/uploads/2020/09/www.cocinayvino.com-la-lechuga-es-mucho-mas-que-agua-lechuga1-e1601317319527-1200x900.jpg'}
-  ]
-  const traerProducto = new Promise ((resolve, reject) =>{
-      let condition = true
-      setTimeout(()=>{
-        if(condition){
-            resolve(productos)
-        }else{
-            reject("F")
-        }
-      }, 2000)
-  })
+    const getItem = ()=>{
+        axios
+          .get('https://apimocha.com/listproduct/products')
+          .then((response)=> setListaProductos(response.data))
+          .catch((err)=>console.log(err))
+      }
+    
+    useEffect(()=>{
+        getItem()
+    }, [])
 
-  useEffect(()=>{
-      traerProducto
-      .then((res)=>setListaProductos(res))
-      .catch((error)=>console.log(error))
-  }, [])
-
-  return (
+    return (
     <div className=' h-full w-screen'>
         <ItemList listaproductos={listaproductos} manageCart={manageCart} stock={stock}/>
     </div>
